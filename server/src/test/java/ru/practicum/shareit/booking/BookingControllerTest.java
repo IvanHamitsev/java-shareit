29c3bc8b48c4d3b1d0b7ddd0ce68efcf3a11a750
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2024-11-24 14:48:05 +0300

    Debug tests

--- server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -83,8 +83,10 @@ class BookingControllerTest {
                 .andExpect(jsonPath("$.id", is(bookingDto.getId()), Long.class))
                 .andExpect(jsonPath("$.booker.name", is(bookingDto.getBooker().getName()), String.class))
                 .andExpect(jsonPath("$.item.name", is(bookingDto.getItem().getName()), String.class))
-                .andExpect(jsonPath("$.start", is(dtf.format(bookingDto.getStart())), String.class))
-                .andExpect(jsonPath("$.end", is(dtf.format(bookingDto.getEnd())), LocalDateTime.class));
+                //.andExpect(jsonPath("$.start", is(dtf.format(bookingDto.getStart())), String.class))
+                .andExpect(jsonPath("$.start", is(bookingDto.getStart()), String.class))
+                //.andExpect(jsonPath("$.end", is(dtf.format(bookingDto.getEnd())), LocalDateTime.class));
+                .andExpect(jsonPath("$.end", is(bookingDto.getEnd()), LocalDateTime.class));
         verify(bookingService, times(1)).createBooking(bookingDto, userId);
     }
 
