86316caaf4af358503cf7c2a7e359b74e7c5ce03
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2024-11-26 23:37:44 +0300

    Debug tests

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,23 +1,21 @@
 package ru.practicum.shareit.booking.dto;
 
-import java.time.LocalDateTime;
-
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookItemRequestDto {
-	private long itemId;
-	@NotNull
-	@FutureOrPresent
-	private LocalDateTime start;
-	@NotNull
-	@Future
-	private LocalDateTime end;
+    private long itemId;
+    @NotNull
+    private LocalDateTime start;
+    @NotNull
+    @Future
+    private LocalDateTime end;
 }
